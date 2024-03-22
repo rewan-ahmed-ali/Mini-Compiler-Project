38db21c7407d26bf0a227faa49a1a0bd43624b36
Rewan Ahmed Abdelghfar <rawan.ahmad43.ra35@gmail.com>
2024-03-23 00:39:29 +0200

    unorderd symbol table

--- unorderd.py
+++ unorderd.py
@@ -6,14 +6,13 @@ def extract_variables(line):
     return variables
 
 # Function to construct the symbol table for a line
-def construct_symbol_table(line, line_number):
+def construct_symbol_table(line, line_number, counter):
     symbol_table = []
     variables = extract_variables(line)
     for var_name, var_declaration in variables:
         data_type = var_declaration.split('=')[0].strip()
         # Assuming no array declarations in this example
         dimension = 0
-        counter = 0
         line_declaration = line_number
         line_reference = []
         symbol_table.append({
@@ -25,16 +24,17 @@ def construct_symbol_table(line, line_number):
             'line_declaration': line_declaration,
             'line_reference': line_reference
         })
-    return symbol_table
+    return symbol_table, counter + 1
 
 # Function to read text file and construct symbol table for each line
 def process_text_file(filename):
     symbol_tables = []
+    counter = 0
     with open(filename, 'r') as file:
         lines = file.readlines()
         for i, line in enumerate(lines):
             line_number = i + 1
-            symbol_table = construct_symbol_table(line.strip(), line_number)
+            symbol_table, counter = construct_symbol_table(line.strip(), line_number, counter)
             symbol_tables.extend(symbol_table)
     return symbol_tables
 
