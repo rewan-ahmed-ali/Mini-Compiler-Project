5efa84644c8e7d7204f99a013f75c857b7557158
Rawan Ahmed Abdelghfar <rewanahmedali16@gmail.com>
2024-04-15 17:37:50 +0200

    Hash Symbol Table

--- hash_table.py
+++ hash_table.py
@@ -1,32 +1,27 @@
 import re
-
 def calculate_hash(variable_name, hash_max):
     variable_length = len(variable_name)  # Calculate the length of the variable name
     ascii_sum = sum(ord(char) for char in variable_name)  # Calculate the sum of ASCII values
     hash_value = (variable_length + ascii_sum) % hash_max  # Calculate the hash value
-    return hash_value  # Return the calculated hash value
+    return hash_value  
 
-# Read from the text file
-file_path = "text.txt"  # Update with your file path
-symbol_table = {}  # Initialize the symbol table
-hash_max = 3  # Example value for hash max
 
-# Open the file and process each line
+file_path = "text.txt"  
+symbol_table = {}  
+hash_max = 3  
 with open(file_path, 'r') as file:
     for line in file:
-        # Remove leading/trailing whitespace
+        # Remove whitespace
         line = line.strip()
         # Ignore empty lines
         if not line:
             continue
-        
         # Extract variable name from the line
         match = re.match(r"^\s*\w+\s+(\w+)\s*=", line)
         if match:
             variable_name = match.group(1)
         else:
-            continue
-        
+            continue        
         # Calculate the hash and store it in the symbol table
         hash_value = calculate_hash(variable_name, hash_max)
         symbol_table[variable_name] = hash_value
