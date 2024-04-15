b5caf7fc5714d47cc92b879ca02605231f822c01
Rawan Ahmed Abdelghfar <rewanahmedali16@gmail.com>
2024-04-15 18:43:56 +0200

    tree

---
+++ t.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def insert(root, value):
+    if root is None:
+        return Node(value)
+    else:
+        if value < root.value:
+            root.left = insert(root.left, value)
+        else:
+            root.right = insert(root.right, value)
+    return root
+
+def print_tree(root, level=0, direction='Root', indent=0):
+    if root is not None:
+        print(' ' * indent + direction + ':', root.value)
+        print_tree(root.left, level + 1, 'Left', indent - 10)
+        print_tree(root.right, level + 1, 'Right', indent + 10)
+
+def main():
+    variables = []
+    with open('text.txt', 'r') as file:
+        for line in file:
+            if line.strip().startswith(('int', 'float')):
+                variable = line.split()[1].strip()
+                variables.append(variable)
+    
+    # Construct the binary search tree
+    root = None
+    for item in variables:
+        root = insert(root, item)
+    print_tree(root, indent=20)
+
+if __name__ == "__main__":
+    main()
