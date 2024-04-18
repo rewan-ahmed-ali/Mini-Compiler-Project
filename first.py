cee29a734417c1f140b8888eddc7d3963dffde5b
Rawan Ahmed Abdelghfar <rewanahmedali16@gmail.com>
2024-04-18 15:12:20 +0200

    First Set

--- first.py
+++ first.py
@@ -1,60 +1,47 @@
-def calculate_first_sets(grammar):
-    first_sets = {}
-    for terminal in terminals(grammar):
-        first_sets[terminal] = {terminal}
-    for non_terminal in non_terminals(grammar):
-        first_sets[non_terminal] = set()
-    while True:
-        changes = False
-        for non_terminal, production in grammar:
-            first_symbol = production[0]
-            if first_symbol in first_sets:
-                if add_first_set(first_sets[non_terminal], first_sets[first_symbol]):
-                    changes = True
-                if 'epsilon' in first_sets[first_symbol]:
-                    index = 1
-                    while index < len(production) and 'epsilon' in first_sets[first_symbol]:
-                        if production[index] in first_sets:
-                            if add_first_set(first_sets[non_terminal], first_sets[production[index]] - {'epsilon'}):
-                                changes = True
-                        index += 1
-        if not changes:
+
+def first(grammar, symbol):
+    first_set = set()
+    symbol_set = {
+        prod[1] for prod in grammar if prod[0] == symbol
+    }
+    for element in symbol_set:
+        if element == 'epsilon':
+            first_set.add(element)
+        elif not element[0].isupper():
+            first_set.add(element[0])
+        else:
+            first_set |= Production_rule_3(grammar, element)
+
+    return first_set
+
+def Production_rule_3(grammar, element):
+    first_set = set()
+    i = 0
+    while i < len(element):
+        first_i = first(grammar, element[i])
+        first_set |= first_i.difference({'epsilon'})
+        if 'epsilon' not in first_i:
             break
-    return first_sets
+        i += 1
+    return first_set
 
-def add_first_set(target_set, new_set):
-    old_size = len(target_set)
-    target_set |= new_set
-    return len(target_set) != old_size
+grammar = [
+    ('E', 'TA'),
+    ('A', '+TA'),
+    ('A', 'epsilon'),
+    ('T', 'FB'),
+    ('B', '*FB'),
+    ('B', 'epsilon'),
+    ('F', '(E)'),
+    ('F', 'd'),
+]
+non_terminals_set = set(left_side for left_side, _ in grammar)
+terminals_set = set()
+for nt in non_terminals_set:
+    terminals_set |= first(grammar, nt).difference({'epsilon'})
+non_terminals = list(non_terminals_set)
+terminals = list(terminals_set)
 
-def non_terminals(grammar):
-    return set(non_terminal for non_terminal, _ in grammar)
-
-def terminals(grammar):
-    terminals_set = set()
-    for _, production in grammar:
-        for symbol in production:
-            if symbol not in non_terminals(grammar):
-                terminals_set.add(symbol)
-    return terminals_set
-
-def main():
-    grammar = [
-        ('E', 'TA'),
-        ('A', '+TA'),
-        ('A', 'epsilon'),
-        ('T', 'FB'),
-        ('B', '*FB'),
-        ('B', 'epsilon'),
-        ('F', '(E)'),
-        ('F', 'd'),
-    ]
-    first_sets = calculate_first_sets(grammar)
-    for non_terminal, first_set in first_sets.items():
-        # Replace "e" with "epsilon"
-        first_set_str = '{' + ', '.join(symbol if symbol != 'e' else 'epsilon' for symbol in first_set) + '}'
-        print(f'First({non_terminal})={first_set_str}')
-
-if __name__ == '__main__':
-
-    main()
+print("First Sets:")
+for nt in non_terminals:
+    print(f'First({nt}) = {first(grammar, nt)}')
